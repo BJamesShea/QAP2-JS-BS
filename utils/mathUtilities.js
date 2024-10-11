64824b555c3a8cb16c8b7df3867576bb0410588a
Brandon Shea <brandon.shea1994@icloud.com>
2024-10-11 15:04:36 -0230

    starting on math questions

--- utils/mathUtilities.js
+++ utils/mathUtilities.js
@@ -1,24 +1,29 @@
 /**
  * Gets a random multiplication, division, subtraction or addition question
- * 
+ *
  * @returns {} The randomly generated math question
  */
 function getQuestion() {
+  // Generating random numbers
+  const num1 = Math.floor(Math.random() * 100);
+  const num2 = Math.floor(Math.random() * 100);
 
+  // Generate a random operatorrrrrrrr
+  const operators = ["+", "-", "*", "/"];
 }
 
 /**
  * Parses the provided question and gets whether or not the provided answer is correct
- * 
+ *
  * @param {*} question The question being answered
  * @param {*} answer The potential answer
  * @returns {boolean} True if the answer was correct, false otherwise.
  */
 function isCorrectAnswer(question, answer) {
-    return false;
+  return false;
 }
 
 module.exports = {
-    getQuestion,
-    isCorrectAnswer
-}
+  getQuestion,
+  isCorrectAnswer,
+};
