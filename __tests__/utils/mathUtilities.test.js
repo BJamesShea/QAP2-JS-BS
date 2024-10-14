1cefa9a31060083e399575b345ea6923fb1db307
Brandon Shea <brandon.shea1994@icloud.com>
2024-10-14 17:30:58 -0230

    final commit

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -1,6 +1,16 @@
 const { isCorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
 
-describe("Tests for getQuestion", () => {});
+describe("Tests for getQuestion", () => {
+  it("Should return an object with first, second and operand", () => {
+    const result = getQuestion();
+
+    expect(typeof result).toBe("object");
+
+    expect(typeof result.first).toBe("number");
+    expect(typeof result.second).toBe("number");
+    expect(["+", "-", "*", "/"]).toContain(result.operand);
+  });
+});
 
 describe("Tests for isCorrectAnswer", () => {
   it("should return true for a correct answer", () => {
