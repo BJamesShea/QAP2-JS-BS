3aec33cc6305a26ab0771881f345733334d90208
Brandon Shea <brandon.shea1994@icloud.com>
2024-10-14 17:21:37 -0230

    starting on testing

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -1,9 +1,11 @@
 const { isCorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
 
-describe("Tests for getQuestion", () => {
-
-});
+describe("Tests for getQuestion", () => {});
 
 describe("Tests for isCorrectAnswer", () => {
-    
+  it("should return true for a correct answer", () => {
+    const question = { first: 2, second: 3, operand: "+" };
+    const result = isCorrectAnswer(question, 5);
+    expect(result).toBe(true);
+  });
 });
